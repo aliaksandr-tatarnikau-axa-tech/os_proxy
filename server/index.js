7590e6e84f018b979bc2277e41c04bb585083605
Oleg Kubrakov <oleg.kubrakov.ags@gmail.com>
2016-05-26 14:12:53 +0800

    https

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const httpProxy = require('http-proxy');
 
 console.log('Proxy starting...');
-httpProxy.createProxyServer({target:'http://10.144.83.162:8445'}).listen(8080);
+httpProxy.createProxyServer({target:'https://10.144.83.162:8445'}).listen(8080);
 console.log('Proxy started');
